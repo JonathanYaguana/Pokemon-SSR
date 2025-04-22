1311d1bb4b31520fdce5ef42f2e05550294f8fa7
JonathanYaguana <jonathansacrox93@gmail.com>
2025-04-22 11:29:58 -0500

    peticiones http

--- src/app/pages/pokemons/pokemons-pages.component.ts
+++ src/app/pages/pokemons/pokemons-pages.component.ts
@@ -1,11 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
+import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
+import { PokemonsService } from '../../pokemons/services/pokemons.service';
 
 @Component({
   selector: 'pokemons-pages',
   standalone: true,
-  imports: [PokemonListComponent],
+  imports: [PokemonListComponent, PokemonListSkeletonComponent],
   templateUrl: './pokemons-pages.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonsPagesComponent { }
+export default class PokemonsPagesComponent implements OnInit {
+
+  private pokemonsService = inject(PokemonsService);
+  public isLoading = signal(true);
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 1500);
+    this.loadPokemons();
+  }
+
+  public loadPokemons( page = 0){
+    this.pokemonsService.loadPage( page ).subscribe( pokemon =>{
+      console.log('On init');
+    });
+  }
+}
