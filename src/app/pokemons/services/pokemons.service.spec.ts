eec1e152bcb2f99a29831ef9f527908ebd30a816
JonathanYaguana <jonathansacrox93@gmail.com>
2025-10-17 11:35:09 -0500

    test(service): prueba si la información no existe

--- src/app/pokemons/services/pokemons.service.spec.ts
+++ src/app/pokemons/services/pokemons.service.spec.ts
@@ -3,6 +3,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { PokemonsService } from './pokemons.service';
 import { TestBed } from '@angular/core/testing';
+import { catchError } from 'rxjs';
 
 const expectedPokemons: SimplePokemon[] = [
   { id: '1', name: 'bulbasaur'},
@@ -70,7 +71,7 @@ describe('PokemonsService', () => {
 
   it('should load a Pokemons by ID', () => {
 
-    const pokemonName = 'bulbasaur';
+    const pokemonName = 'PokemonNoE';
 
     service.loadPokemon(pokemonName).subscribe((pokemon:any) => {
       expect(pokemon).toEqual(mockPokemon);
@@ -85,4 +86,29 @@ describe('PokemonsService', () => {
     req.flush(mockPokemon);
   });
 
+  it('should catch error if pokémon not found', () => {
+
+    const pokemonName = 'pokemon-no-existe';
+
+    service.loadPokemon(pokemonName)
+    .pipe(
+      catchError((err) => {
+        //console.log(err);
+        expect(err.message).toContain('Pokemon not found');
+        return [];
+      })
+    )
+    .subscribe();
+
+    const req = httpMock.expectOne(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush('Pokemon not found', {
+      status: 404,
+      statusText: 'Not Found'
+    });
+  });
 });
